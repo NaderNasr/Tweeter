d9c1c810eb1bee5df9a64e31c502b5c8c5efecfc
Nader Nasr <nadernasr7@gmail.com>
2022-02-02 10:00:16 -0500

    dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,17 +1,35 @@
+/* eslint-disable no-undef */
 $(document).ready(function() {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const createdAt = timeago.format(data.created_at);
 
   const createTweetElement = (tweetData) => {
+    
     const tweet = $(`
   <div class="tweet-box">
   <div class="box">
@@ -27,7 +45,7 @@ $(document).ready(function() {
 <hr class="tweet-divider"/>
 <div class="box">
   <div class="content-left">
-    <p>${tweetData.created_at}</p>
+    <p>${createdAt}</p>
   </div>
   <div class="content-right">
     <i class="fas fa-flag icon"></i>
@@ -40,7 +58,16 @@ $(document).ready(function() {
     return tweet;
   };
 
-  const $tweet = createTweetElement(tweetData);
+
+  const renderTweet = (data) => {
+    for (let user of data) {
+      const showTweet = createTweetElement(user);
+      $('#tweets-container').prepend(showTweet);
+    }
+  };
+
+  const $tweet = renderTweet(data);
   // to add it to the page so we can make sure it's got all the right elements, classes, etc.
   $('#tweets-container').append($tweet);
+  
 });
